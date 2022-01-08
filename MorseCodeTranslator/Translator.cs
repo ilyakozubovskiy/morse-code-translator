3f707acb594c209668bee3428b0e2cfaee77c494
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-01-08 18:10:00 +0300

    Morse Code Translator

--- MorseCodeTranslator/Translator.cs
+++ MorseCodeTranslator/Translator.cs
@@ -9,26 +9,100 @@ namespace MorseCodeTranslator
     {
         public static string TranslateToMorse(string message)
         {
-            // TODO #1. Implement the method using StringBuilder, and MorseCodes.CodeTable array.
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+            WriteMorse(MorseCodes.CodeTable, message, result);
+
+            return result.ToString();
         }
 
         public static string TranslateToText(string morseMessage)
         {
-            // TODO #2. Implement the method using StringBuilder, and MorseCodes.CodeTable array.
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+            WriteText(MorseCodes.CodeTable, morseMessage, result);
+
+            return result.ToString();
         }
 
         public static void WriteMorse(char[][] codeTable, string message, StringBuilder morseMessageBuilder, char dot = '.', char dash = '-', char separator = ' ')
         {
-            // TODO #3. Implement the method.
-            throw new NotImplementedException();
+            if (codeTable == null)
+            {
+                throw new ArgumentNullException(nameof(codeTable));
+            }
+
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            if (morseMessageBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(morseMessageBuilder));
+            }
+
+            message = message.ToUpperInvariant();
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == dot || message[i] == ',' || message[i] == ' ')
+                {
+                    continue;
+                }
+
+                if (i > 0)
+                {
+                    morseMessageBuilder.Append(" ");
+                }
+
+                morseMessageBuilder.Append(codeTable[message[i] - 65][1..]);
+            }
+
+            morseMessageBuilder.Replace('.', dot);
+            morseMessageBuilder.Replace('-', dash);
+            morseMessageBuilder.Replace(' ', separator);
         }
 
         public static void WriteText(char[][] codeTable, string morseMessage, StringBuilder messageBuilder, char dot = '.', char dash = '-', char separator = ' ')
         {
-            // TODO #4. Implement the method.
-            throw new NotImplementedException();
+            if (codeTable == null)
+            {
+                throw new ArgumentNullException(nameof(codeTable));
+            }
+
+            if (morseMessage == null)
+            {
+                throw new ArgumentNullException(nameof(morseMessage));
+            }
+
+            if (messageBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(messageBuilder));
+            }
+
+            morseMessage = morseMessage.Replace(separator, ' ');
+            morseMessage = morseMessage.Replace(dot, '.');
+            morseMessage = morseMessage.Replace(dash, '-');
+
+            for (int i = 0; i < morseMessage.Length; i++)
+            {
+                int beginIndex = i;
+                int endIndex = morseMessage.IndexOf(' ', beginIndex);
+                endIndex = endIndex < 0 ? morseMessage.Length : endIndex;
+
+                string phrase = morseMessage[beginIndex..endIndex];
+                for (int j = 0; j < codeTable.GetLength(0); j++)
+                {
+                    string phraseInTable = string.Join(string.Empty, codeTable[j][1..]);
+
+                    if (phrase == phraseInTable)
+                    {
+                        messageBuilder.Append(codeTable[j][0]);
+                        break;
+                    }
+                }
+
+                i = endIndex;
+            }
         }
     }
 }
